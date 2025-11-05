029bce00254df6aef3ee388b0dc73414ef91e673
perdanaqing-1120 <perdana.qing@gmail.com>
2025-11-05 15:51:12 +0800

    her

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const colorInput = document.getElementById("colorInput");
+const colorInputBody = document.getElementById("colorInputBody");
+const colorInputNavbar = document.getElementById("colorInputNavbar");
+const colorInput = document.getElementById("colorInput")
+
+const backgroundColorBody = document.querySelectorAll(".background-color-body")
+const backgroundColorNavbar = document.querySelectorAll(".background-color-navbar")
 const backgroundColor = document.querySelectorAll(".background-color")
 
-function colorSubmit(){
-    console.log(colorInput.value);
+function colorSubmitBody() {
+    backgroundColorBody.forEach(backgroundColorBody => {
+        backgroundColorBody.style.backgroundColor = colorInputBody.value
+    })
+}
 
+function resetColorBody() {
+    backgroundColorBody.forEach(backgroundColorBody => {
+        backgroundColorBody.style.backgroundColor = "aliceblue"
+    })
+}
+
+function colorSubmitNavbar() {
+    backgroundColorNavbar.forEach(backgroundColorNavbar => {
+        backgroundColorNavbar.style.backgroundColor = colorInputNavbar.value
+        // Body.style.backgroundColor = colorInputNavbar.value
+    })
+
+}
+
+function resetColorNavbar() {
+    backgroundColorNavbar.forEach(backgroundColorNavbar => {
+        backgroundColorNavbar.style.backgroundColor = "#2279fd"
+    })
+}
+
+function colorSubmit() {
     backgroundColor.forEach(backgroundColor => {
         backgroundColor.style.backgroundColor = colorInput.value
+    })
+}
+
+function resetColor() {
+    backgroundColor.forEach(backgroundColor => {
+        backgroundColor.style.backgroundColor = "#76a8f3"
     })
 }
